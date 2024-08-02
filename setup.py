edef7714e667ebfce72974eef2bdd6c94994dca0
Neeraj Bhandari <neerajbhandari1193@gmail.com>
2024-08-01 21:55:38 -0700

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI project",
+    version= '0.0.0',
+    author = "Neeraj Bhandari",
+    author_email = "neerajbhandari1193@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+)
